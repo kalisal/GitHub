e272742fdcdac3c167359f401f1cd587700c2600
kalisal <lee.kalisa@csmstudents.org>
2019-05-16 16:29:23 +0000

    Adding more

--- gitlab.py
+++ gitlab.py
@@ -3,5 +3,10 @@ print("Hope this works")
 num=int(input("Enter your favorite number"))
 print(num)
 print("How can I get some more money")
-point= int(input("At what point do you leave?"))
+point= int(input("At what point do you stay?"))
 print("Hope works,")
+print("Ok it works now")
+if(num == 0):
+    print("That is your favorite number")
+else:
+    print("I like that number")
