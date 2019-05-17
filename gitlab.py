f51af700e8629b32ccc6beb47ee4d4d4f0d93c9c
kalisal <lee.kalisa@csmstudents.org>
2019-05-17 15:23:06 +0000

    Hello

--- gitlab.py
+++ gitlab.py
@@ -1,7 +1,7 @@
 print("Hope this works")
 num=int(input("Enter your favorite number"))
 print(num)
-print("Hope works,")
+print("Hope works!")
 if(num == 0):
     print("That is your favorite number")
 else:
