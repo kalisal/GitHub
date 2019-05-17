0be4b2ba45d88afd0362a49933bd5a3c7b1d6ce1
kalisal <lee.kalisa@csmstudents.org>
2019-05-17 16:10:56 +0000

    12

--- gitlab.py
+++ gitlab.py
@@ -1,9 +1,1 @@
-num=int(input("Enter your favorite number"))
-print(num)
-if(num == 0):
-    print("That is your favorite number")
-else:
-    print("I like that number")
-print("This is a test one") 
-print("HEllO")
 print("YEEEEEEHAWWWWWWWWW")
