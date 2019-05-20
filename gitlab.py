30cca6742f4b9cf3a4c7c79c61c94e50b179d7fe
kalisal <lee.kalisa@csmstudents.org>
2019-05-20 03:00:46 +0000

    Adding Changes

--- gitlab.py
+++ gitlab.py
@@ -4,5 +4,6 @@ while (num != -1):
     print ("You entered: " + str(num))
     sum= sum + num
     num = int(input("Enter a number, -1 to stop"))
-print(sum)    
+print(sum)
 print ("Done.")
+print("HOLA GIRL")
